5233722a445614c42fbb999ced3193e69403f7ad
Z3RO4l <zordzraplha@gmail.com>
2025-07-24 15:50:03 +0000

    FASE 2 CHECK

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,61 +1,47 @@
 #include <stdio.h>
 
+#define proporcao 10 
+#define navios 3
+
+
 int main() {
-    int tabuleiro[10][10] = {
-        {0,0,0,0,0,0,0,0,0,0},  // Linha 1
-        {0,0,0,0,0,0,0,0,0,0},  // Linha 2
-        {0,0,0,3,3,3,0,0,0,0},  // Linha 3
-        {0,0,0,0,0,0,0,0,0,0},  // Linha 4
-        {0,0,0,0,0,0,0,0,0,0},  // Linha 5
-        {0,0,0,0,0,0,0,3,0,0},  // Linha 6
-        {0,0,0,0,0,0,0,3,0,0},  // Linha 7
-        {0,0,0,0,0,0,0,3,0,0},  // Linha 8
-        {0,0,0,0,0,0,0,0,0,0},  // Linha 9
-        {0,0,0,0,0,0,0,0,0,0}   // Linha 10
-    };
-            printf("TABULEIRO DE BATALHA NAVAL\n");
-    // Imprimindo o cabeçalho com letras
-    printf("  A B C D E F G H I J\n");
 
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", i + 1);  // Número da linha
-        if (i + 1 < 10) printf(" ");  // Alinhamento extra para linhas de 1 dígito
+    int TABULEIRO [proporcao][navios];
 
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+    for (int i = 0; i < proporcao; i++){
+        for (int j = 0; j < proporcao; j++){
+            TABULEIRO[i][j] = 0;
+        }
+        
+    }//navio 1 na horizontal//
+    for (int j = 1; j < 4; j++){
+        TABULEIRO[2][j] = navios;
+
+    }//NAVIO 2 VERTICAL//
+         for (int i = 4; i < 5; i++){
+            TABULEIRO[i][6] = navios;
+
+    }//NAVIO 3 DIAGONAL PRINCIPAL//
+             for (int i = 0; i < 9 ; i++){
+                 TABULEIRO[i][i] = navios;
+                 
+    }//NAVIO 4 DIAGONAL SECUNDARIA//
+                     for (int i = 0; i < 3; i++){
+                         TABULEIRO[i][9 - 1] = navios;
+    }
+    
+   printf("TABULEIRO DE BATALHA NAVAL\n");
+  
+      printf("   A B C D E F G H I J\n");
+
+    for (int i = 0; i < proporcao; i++) {
+          printf("%d ", i + 1);  
+        if (i + 1 < 10) printf(" ");  
+         for (int j = 0; j < proporcao; j++) {
+            printf("%d ", TABULEIRO[i][j]);
         }
         printf("\n");
     }
-    
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
